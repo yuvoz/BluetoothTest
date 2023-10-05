d5aff9d076f2e322ebf9b1bed1270c37e0c4e75d
yuvoz <yuvoz@ciklum.com>
2023-10-05 19:08:05 -0400

    Added BluetoothTest project
    Added BluetoothTest project

---
+++ BluetoothTest/BluetoothTest/BluetoothTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothTestApp.swift
+//  BluetoothTest
+//
+//  Created by Yuriy on 05.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct BluetoothTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
